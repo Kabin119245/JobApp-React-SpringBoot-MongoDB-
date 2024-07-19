e2e397f196fc0646fbf8a7af97279c54f20acefc
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-07-19 19:25:21 +0545

    backend peoject setup

---
+++ joblisting-backend/src/main/java/com/job/joblisting/JoblistingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.job.joblisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoblistingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoblistingBackendApplication.class, args);
+	}
+
+}
---
+++ joblisting-backend/src/test/java/com/job/joblisting/JoblistingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job.joblisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoblistingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
