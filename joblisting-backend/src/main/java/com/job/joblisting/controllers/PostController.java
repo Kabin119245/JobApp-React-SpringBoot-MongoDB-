e300e22deb9c3aba15e46b5df5f644f1e3ffc720
Kabin119245 <kabin.077bct043@acem.edu.np>
2024-07-19 23:33:20 +0545

    implemented mongodb atlas search

--- joblisting-backend/src/main/java/com/job/joblisting/controllers/PostController.java
+++ joblisting-backend/src/main/java/com/job/joblisting/controllers/PostController.java
@@ -3,10 +3,10 @@ package com.job.joblisting.controllers;
 
 import com.job.joblisting.models.Post;
 import com.job.joblisting.repositories.PostRepository;
+import com.job.joblisting.repositories.SearchRepository;
+import com.job.joblisting.repositories.SearchRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,6 +16,9 @@ public class PostController {
     @Autowired
     PostRepository repo;
 
+    @Autowired
+    SearchRepository searchrepo;
+
 
     //fetch all jobs
     @GetMapping("/posts")
@@ -23,7 +26,15 @@ public class PostController {
      return repo.findAll();
     }
 
+    // fetch searched jobs
+
+    @GetMapping("/posts/{text}")
+    public List<Post> search(@PathVariable String text){
+        return searchrepo.findByText(text);
+    }
+
     //posting job
+    @PostMapping("/posts")
     public Post addPost(@RequestBody Post post) {
         return repo.save(post);
     }
